8e1908fc44e3b67f8f4675295c9d0496cbec3860
Eugene Sinitsyn <e.sinitsyn.s@gmail.com>
2021-06-27 09:49:52 +0300

    Install material packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { RootComponent } from './root.component';
 import { routes } from './config/router.config';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [RootComponent],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    BrowserAnimationsModule
   ],
   bootstrap: [RootComponent]
 })
